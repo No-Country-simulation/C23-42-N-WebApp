c6d2f3542f57b78885e5623032cdf00019e15654
amoroccoire <amoroccoire@unsa.edu.pe>
2025-01-16 15:29:35 -0500

    controller de ejemplo

--- server/src/main/java/org/nctry/server/demo/controller/DemoController.java
+++ server/src/main/java/org/nctry/server/demo/controller/DemoController.java
@@ -1,4 +1,20 @@
 package org.nctry.server.demo.controller;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class DemoController {
+
+    @GetMapping("/demo")
+    public String welcome() {
+        return "Bienvenido";
+    }
+
 }
