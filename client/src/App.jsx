bc1083ea37591cd1a1de6135c2a8411f233d0e56
xavier77dev <xavier.basurto.77@gmail.com>
2025-01-15 20:30:02 -0500

    config: configuration project packages installed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Button } from "./components/ui/button";
+import { useStore } from "./store/useExampleStore.js";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const bears = useStore((state) => state.bears);
+  const increasePopulation = useStore((state) => state.increasePopulation);
+  const removeAllBears = useStore((state) => state.removeAllBears);
+  const updateBears = useStore((state) => state.updateBears);
 
+  console.log(bears);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>hello world</h1>
+      <span className="text-3xl">{bears}</span>
+      <Button onClick={increasePopulation}>buton</Button>
+      <Button onClick={removeAllBears}>resetear</Button>
+      <Button onClick={() => updateBears(77)}>actualizar numero a 77</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
