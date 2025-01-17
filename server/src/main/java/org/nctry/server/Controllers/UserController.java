36f74b87ec1259b35a852ee8a75a5fb82fb8ade7
NahuelGuillermoViera <nahuelguillermoviera@gmail.com>
2025-01-17 18:17:30 -0300

    Add Song Service

--- server/src/main/java/org/nctry/server/Controllers/UserController.java
+++ server/src/main/java/org/nctry/server/Controllers/UserController.java
@@ -2,13 +2,9 @@ package org.nctry.server.Controllers;
 
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
-import java.util.Set;
 
 import static org.nctry.server.Controllers.ApiPaths.ROOT;
 
@@ -50,4 +46,6 @@ public class UserController {
        */
         return null;
     }
+
+
 }
---
+++ server/src/main/java/org/nctry/server/services/Song/SongServiceImpl.java
@@ -0,0 +1,85 @@
+package org.nctry.server.services.Song;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SongServiceImpl implements SongService {
+
+    public /*SongDTO*/ void saveSong(/*SongDTO songDTO*/) {
+        //Si la song no existe, crea el objeto, de lo contrario actualiza el objeto
+        /*
+            TODO:
+                Condicional de existencias
+                Retorna el DTO del objeto creado o actualizado
+        */
+    }
+
+
+    public void deleteSongById(Long id) {
+        /*
+            TODO:
+                Eliminar Song por Id en caso de que exista
+                Devolver un 404 en caso de que no exista
+                Devolver un 403 en caso de no estar autorizado
+        */
+    }
+
+    public void deleteSong(/*SongDTO songDTO*/){
+        /*
+            TODO:
+             Obtener mapear DTO a Song,
+             Si no existe, error 404
+             Si no autorizado, error 403
+             Si existe y esta autorizado eliminar
+        */
+    }
+
+    public /*SongDTO*/ void getSongById(Long id) {
+        /*
+            TODO:
+                Obtengo la Song por el id,
+                Si no existe devuelvo un error 404,
+                Si existe aplico MapToDTO y devuelvo el DTO del objeto
+        */
+    }
+
+    public /*List<SongDTO>*/ void getAllSong() {
+        /*
+            TODO:
+             Si la lista esta vacia, devolver 404
+             Devolver todas las Songs
+        */
+    }
+
+    public /*List<SongDTO>*/ void getAllSongByName(String name) {
+        /*
+            TODO:
+                Mismo manejo de que getAllSong
+        */
+    }
+
+    public /*List<SongDTO>*/ void getAllSongByGenre(String genre) {
+        /*
+            TODO:
+                Mismo manejo de que getAllSong
+        */
+    }
+
+    public void likeSong(Long userId, Long songId) {
+        /*
+            TODO:
+                Vincular cada cancion a 1 like por usuario
+                Aumentar el contador de likes de la publicacion en 1 por cada vez que se le de
+        */
+    }
+
+
+
+    /*
+        TODO:
+         MapToDTO
+         MapToSong
+            Condicional si el objeto existe o no (boolean)
+    */
+
+}
