1af33acf84f3a81e8eacc5b7cabebb00e81eccb3
amoroccoire <amoroccoire@unsa.edu.pe>
2025-01-19 18:03:23 -0500

    verificacion de usuario con codigo, reenvio de codigo de verificacion, envio de codigo de verificacion, generacion de codigo de verificacion e integracion de email con register

--- server/src/main/java/org/nctry/server/auth/services/AuthService.java
+++ server/src/main/java/org/nctry/server/auth/services/AuthService.java
@@ -1,11 +1,17 @@
 package org.nctry.server.auth.services;
 
+import jakarta.mail.MessagingException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.nctry.server.auth.dto.AuthResponse;
 import org.nctry.server.auth.dto.LoginRequest;
 import org.nctry.server.auth.dto.RegisterRequest;
+import org.nctry.server.auth.dto.VerifyUser;
+import org.nctry.server.email.EmailService;
 import org.nctry.server.user.UserDetailsWrapper;
-import org.nctry.server.user.entity.User;
+import org.nctry.server.user.enums.Country;
+import org.nctry.server.user.model.User;
+import org.nctry.server.user.model.UserFulldata;
 import org.nctry.server.user.repository.UserRepository;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -13,6 +19,10 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
@@ -23,8 +33,11 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
 
+    private final EmailService emailService;
+
     private UserDetailsWrapper userDetailsWrapper;
 
+    @Transactional
     public AuthResponse login(LoginRequest request) {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
         User user = userRepository.findByUsername(request.getUsername()).orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
@@ -42,14 +55,45 @@ public class AuthService {
                 .build();
     }
 
-    public AuthResponse register(RegisterRequest request) {
+    @Transactional
+    public AuthResponse register(RegisterRequest request) { //agregar verificaicon email
+
+        Optional<User> resUser = userRepository.findByUsernameOrUserFullData_Email(request.getUsername(), request.getEmail());
+
+        if (resUser.isPresent())
+            throw new RuntimeException("User already exists"); //aqui va excepcion personalizada
+
+        String countryName = request.getCountry();
+        Country country;
+
+        try {
+            if (countryName == null || countryName.isEmpty())
+                throw new IllegalArgumentException("Country cannot be null or empty");
+
+            //Verificar si el pais esta en el ENUM
+            country = Country.valueOf(countryName.toUpperCase());
+
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Invalid country: " + countryName);
+        }
+
+        UserFulldata userFulldata = UserFulldata.builder()
+                .email(request.getEmail())
+                .birthday(request.getBirthday())
+                .country(country)
+                .role(1)
+                .confirmationCode(generateVerificationCode())
+                .verificationCodeExpiresAt(LocalDateTime.now().plusMinutes(15))
+                .enabled(false)
+                .build();
+
         User user = User.builder()
                 .username(request.getUsername())
-                .email(request.getEmail())
+                .userFullData(userFulldata)
                 .password(passwordEncoder.encode(request.getPassword()))
-                .country(request.getCountry())
-                .role(1)
                 .build();
+
+        sendVerificationEmail(user);
 
         userRepository.save(user);
 
@@ -58,5 +102,75 @@ public class AuthService {
         return AuthResponse.builder()
                 .token(jwtService.getToken(userDetailsWrapper))
                 .build();
+    }
+
+    public void verifyUser(VerifyUser verifyUser) {
+        Optional<User> optionalUser = userRepository.findByUserFullData_Email(verifyUser.getEmail());
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            if (user.getUserFullData().getVerificationCodeExpiresAt().isBefore(LocalDateTime.now()))
+                throw new RuntimeException("EL codigo de verificación ha expirado");
+
+            if (user.getUserFullData().getConfirmationCode().equals(verifyUser.getVerificationCode())) {
+                user.getUserFullData().setEnabled(true);
+                user.getUserFullData().setConfirmationCode(null);
+                user.getUserFullData().setVerificationCodeExpiresAt(null);
+                userRepository.save(user);
+            }
+            else {
+                throw new RuntimeException("Codigo de verificación invalido");
+            }
+        }
+        else {
+            throw new RuntimeException("Usuario no encontrado");
+        }
+    }
+
+    public void resendVerificationCode(String email) {
+        Optional<User> optionalUser = userRepository.findByUserFullData_Email(email);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+
+            if (user.getUserFullData().isEnabled()) {
+                throw new RuntimeException("La cuenta esta verificada");
+            }
+            user.getUserFullData().setConfirmationCode(generateVerificationCode());
+            user.getUserFullData().setVerificationCodeExpiresAt(LocalDateTime.now().plusMinutes(5));
+            sendVerificationEmail(user);
+            userRepository.save(user);
+        }
+        else {
+            throw new RuntimeException("Usuario no encontrado");
+        }
+    }
+
+    public void sendVerificationEmail(User user) {
+        String subject = "Account verification";
+        String verificationCode = user.getUserFullData().getConfirmationCode();
+        String htmlMessage = "<html>"
+                + "<body style=\"font-family: Arial, sans-serif;\""
+                + "<div style=\"background-color: #f5f5f5; padding: 20px;\">"
+                + "<h2 style=\"color: #333;\">Bienvenido!</h2>"
+                + "<p style=\"font-size: 16px;\">Por favor ingrese el codigo de verificación para continuar:</p>"
+                + "<div style=\"background-color: #fff; padding: 20px; border-radius: 5px; box-shadow: 0 0 10px rgba(0,0,0,0.1);\">"
+                + "<h3 style=\"color: #333;\">Codigo de verificación:</h3>"
+                + "<p style=\"font-size: 18px; font-weight: bold; color: #007bff;\">" + verificationCode + "</p>"
+                + "</div>"
+                + "</div>"
+                + "</body>"
+                + "</html>";
+
+        try {
+            emailService.sendVerificationEmail(user.getUserFullData().getEmail(), subject, htmlMessage);
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String generateVerificationCode() {
+        Random random = new Random();
+        int code = random.nextInt(900000) + 100000;
+        return String.valueOf(code);
     }
 }
