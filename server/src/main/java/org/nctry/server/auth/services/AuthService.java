4d19af70fd0e1ec4ea08a43665ee08114a004d2d
amoroccoire <amoroccoire@unsa.edu.pe>
2025-01-19 00:45:12 -0500

    envolviendo user en el UserDetailsWrapper

--- server/src/main/java/org/nctry/server/auth/services/AuthService.java
+++ server/src/main/java/org/nctry/server/auth/services/AuthService.java
@@ -4,15 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.nctry.server.auth.dto.AuthResponse;
 import org.nctry.server.auth.dto.LoginRequest;
 import org.nctry.server.auth.dto.RegisterRequest;
-import org.nctry.server.user.entity.Role;
+import org.nctry.server.user.UserDetailsWrapper;
 import org.nctry.server.user.entity.User;
 import org.nctry.server.user.repository.UserRepository;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -26,16 +23,20 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
 
+    private UserDetailsWrapper userDetailsWrapper;
+
     public AuthResponse login(LoginRequest request) {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
-        UserDetails user = userRepository.findByUserName(request.getUsername()).orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+        User user = userRepository.findByUsername(request.getUsername()).orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
 
         // Verifica que la contraseña ingresada coincide con la almacenada
         if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
             throw new BadCredentialsException("Contraseña incorrecta");
         }
 
-        String token = jwtService.getToken(user);
+        userDetailsWrapper = new UserDetailsWrapper(user);
+
+        String token = jwtService.getToken(userDetailsWrapper);
         return AuthResponse.builder()
                 .token(token)
                 .build();
@@ -43,17 +44,19 @@ public class AuthService {
 
     public AuthResponse register(RegisterRequest request) {
         User user = User.builder()
-                .userName(request.getUsername())
+                .username(request.getUsername())
                 .email(request.getEmail())
                 .password(passwordEncoder.encode(request.getPassword()))
                 .country(request.getCountry())
-                .role(Role.USER)
+                .role(1)
                 .build();
 
         userRepository.save(user);
 
+        userDetailsWrapper = new UserDetailsWrapper(user);
+
         return AuthResponse.builder()
-                .token(jwtService.getToken(user))
+                .token(jwtService.getToken(userDetailsWrapper))
                 .build();
     }
 }
