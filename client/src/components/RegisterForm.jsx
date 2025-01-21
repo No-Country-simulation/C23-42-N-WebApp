3f875b3855b3aeb01065c05d2b9fee2c6678e0bd
xavier77dev <xavier.basurto.77@gmail.com>
2025-01-21 18:38:13 -0500

    change: modify style componente RegisterForm

--- client/src/components/RegisterForm.jsx
+++ client/src/components/RegisterForm.jsx
@@ -98,136 +98,146 @@ export default function RegisterForm() {
   }
 
   return (
-    <div className="flex min-h-screen items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <Card className="w-full max-w-md">
-        <CardHeader className="space-y-2 text-center">
-          <div className="flex justify-center">
-            <Link to="/">
-              <img
-                src="/logo-musync3.png"
-                alt="Logo"
-                width={48}
-                height={48}
-                className="h-12 w-12"
-              />
-            </Link>
-          </div>
-          <h1 className="text-2xl font-bold tracking-tight text-orangePrimary">
-            Crear cuenta
-          </h1>
-          <p className="text-sm text-gray-600">
-            ¿Ya tienes una cuenta?{" "}
-            <Link
-              to="/login"
-              className="text-indigo-500 font-bold  hover:underline"
-            >
-              Inicia sesión
-            </Link>
-          </p>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(onSubmit)}
-              className="space-y-4"
-              noValidate
-            >
-              <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-left w-full block font-medium">
-                      Nombre de usuario
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        className="focus:bg-white focus:text-black text-black placeholder:text-red-500 placeholder:opacity-50"
-                        placeholder="john doe"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-left w-full block font-medium">
-                      Email
-                      <span className="text-red-500"> *</span>
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        type="email"
-                        {...field}
-                        className="placeholder:text-gray-500 placeholder:opacity-50"
-                        placeholder="jhon@example.com"
-                      />
-                    </FormControl>
-                    <FormMessage className="text-left w-full block font-medium" />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-left w-full block font-medium">
-                      Contraseña
-                      <span className="text-red-500"> *</span>
-                    </FormLabel>
-                    <FormControl>
-                      <Input type="password" {...field} minLength={6} />
-                    </FormControl>
-                    <FormMessage className="text-left w-full block font-medium" />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="confirmPassword"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-left w-full block font-medium">
-                      Confirmar Contraseña
-                      <span className="text-red-500"> *</span>
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        type="password"
-                        {...field}
-                        // pattern={password}
-                        // onInvalid={(e) => {
-                        //   e.target.setCustomValidity(
-                        //     "Las contraseñas no coinciden",
-                        //   );
-                        // }}
-                        // onInput={(e) => {
-                        //   e.target.setCustomValidity("");
-                        // }}
-                      />
-                    </FormControl>
-                    <FormMessage className="text-left w-full block font-medium" />
-                  </FormItem>
-                )}
-              />
-              <Button
-                type="submit"
-                variant="btnOrange"
-                className="w-full"
-                disabled={isLoading}
+    <div className="flex flex-col lg:flex-row min-h-screen h-screen items-center justify-center py-12 px-4 sm:px-6 lg:px-8 w-full gap-4 bg-orangePrimary/10">
+      <div className="w-full flex justify-center">
+        <Card className="w-[500px]">
+          <CardHeader className="space-y-2 text-center">
+            <div className="flex justify-center">
+              <Link to="/">
+                <img
+                  src="/logo-musync3.png"
+                  alt="Logo"
+                  width={48}
+                  height={48}
+                  className="h-12 w-12"
+                />
+              </Link>
+            </div>
+            <h1 className="text-2xl font-bold tracking-tight text-orangePrimary">
+              Crear cuenta
+            </h1>
+            <p className="text-sm text-gray-600">
+              ¿Ya tienes una cuenta?{" "}
+              <Link
+                to="/login"
+                className="text-indigo-500 font-bold  hover:underline"
               >
-                {isLoading ? "Registrando..." : "Registrarse"}
-              </Button>
-            </form>
-          </Form>
-        </CardContent>
-      </Card>
+                Inicia sesión
+              </Link>
+            </p>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-4"
+                noValidate
+              >
+                <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-left w-full block font-medium">
+                        Nombre de usuario
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          className="focus:bg-white focus:text-black text-black placeholder:text-red-500 placeholder:opacity-50"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-left w-full block font-medium">
+                        Email
+                        <span className="text-red-500"> *</span>
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          type="email"
+                          {...field}
+                          className="placeholder:text-gray-500 placeholder:opacity-50"
+                        />
+                      </FormControl>
+                      <FormMessage
+                        className={`text-left w-full block font-medium ${form.formState.errors.confirmPassword ? "" : "text-gray-500"} `}
+                      >
+                        Ingresar un email: ejemplo@gmail.com
+                      </FormMessage>
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-left w-full block font-medium">
+                        Contraseña
+                        <span className="text-red-500"> *</span>
+                      </FormLabel>
+                      <FormControl>
+                        <Input type="password" {...field} minLength={6} />
+                      </FormControl>
+                      <FormMessage
+                        className={`text-left w-full block font-medium ${form.formState.errors.confirmPassword ? "" : "text-gray-500"}`}
+                      >
+                        Al menos 8 caracteres: una letra mayúscula, minúscula y
+                        un número
+                      </FormMessage>
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="confirmPassword"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-left w-full block font-medium">
+                        Confirmar Contraseña
+                        <span className="text-red-500"> *</span>
+                      </FormLabel>
+                      <FormControl>
+                        <Input type="password" {...field} />
+                      </FormControl>
+                      <FormMessage
+                        className={`text-left w-full block font-medium ${form.formState.errors.confirmPassword ? "" : "text-gray-500"}`}
+                      >
+                        Ingresar nuevamente la contraseña
+                      </FormMessage>
+                    </FormItem>
+                  )}
+                />
+                <Button
+                  type="submit"
+                  variant="btnOrange"
+                  className="w-full"
+                  disabled={isLoading}
+                >
+                  {isLoading ? "Registrando..." : "Registrarse"}
+                </Button>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      </div>
+
+      <div className="hidden lg:flex flex-col items-center justify-center p-8 w-full">
+        <div className="relative w-80 h-80 rounded-full bg-white shadow-2xl flex items-center justify-center overflow-hidden">
+          <img src="/images/musync1_720.jpg" />
+        </div>
+        <p className="mt-8 text-lg text-center text-muted-foreground max-w-md">
+          Conecta, chatea, publica, comparte y sincroniza tu música con el mundo
+        </p>
+      </div>
     </div>
   );
 }
