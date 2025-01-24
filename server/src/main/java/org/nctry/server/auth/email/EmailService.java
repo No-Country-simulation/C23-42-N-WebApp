c693f44e3bed0983cd36e9a018be025fa15cf63a
amoroccoire <amoroccoire@unsa.edu.pe>
2025-01-24 17:10:29 -0500

    email inhabilitado

--- server/src/main/java/org/nctry/server/auth/email/EmailService.java
+++ server/src/main/java/org/nctry/server/auth/email/EmailService.java
@@ -1,15 +1,16 @@
-package org.nctry.server.email;
+package org.nctry.server.auth.email;
 
+/*
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Service;*/
 
-@Service
+//@Service
 public class EmailService {
-
+/*
     @Autowired
     private JavaMailSender emailSender;
 
@@ -22,5 +23,5 @@ public class EmailService {
         helper.setText(text, true);
 
         emailSender.send(message);
-    }
+    }*/
 }
