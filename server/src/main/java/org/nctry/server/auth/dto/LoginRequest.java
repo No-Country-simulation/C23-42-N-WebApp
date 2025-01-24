2ecc0d62d8f232a503c4158e7079121ff5dbc845
amoroccoire <amoroccoire@unsa.edu.pe>
2025-01-24 17:12:51 -0500

    mejorando validaciones para los json

--- server/src/main/java/org/nctry/server/auth/dto/LoginRequest.java
+++ server/src/main/java/org/nctry/server/auth/dto/LoginRequest.java
@@ -1,5 +1,6 @@
 package org.nctry.server.auth.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
@@ -10,9 +11,12 @@ import lombok.*;
 @Builder
 public class LoginRequest {
     @NotBlank(message = "¡El campo usuario no puede estar vacio!")
-    @Pattern(regexp = "^[a-zA-Z0-9\\-_]+$", message = "¡Sólo caracteres alfanúmericos y los caracteres especiales: - _ son aceptados!")
+    @Email(message = "Ingrese un email valido")
     String email;
     @NotBlank(message = "¡El campo contraseña no puede estar vacio!")
-    @Pattern(regexp = "^[a-zA-Z0-9\\-_.:]+$", message = "¡Sólo caracteres alfanúmericos y los caracteres especiales: - _ . : son aceptados!")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z0-9\\-_.:!@#$%^&*]{8,16}$",
+            message = "La contraseña debe tener entre 8 y 20 caracteres, al menos una letra mayúscula, una letra minúscula, un dígito, y sólo caracteres alfanuméricos o los especiales: - _ . : ! @ # $ % ^ & *"
+    )
     String password;
 }
