7b5219259db54b6320e733af00d62d00b6ca9b8e
amoroccoire <amoroccoire@unsa.edu.pe>
2025-01-23 20:50:59 -0500

    modificaciones menores

--- server/src/main/java/org/nctry/server/user/repository/IUserRepository.java
+++ server/src/main/java/org/nctry/server/user/repository/IUserRepository.java
@@ -2,6 +2,20 @@ package org.nctry.server.user.repository;
 
 import org.nctry.server.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface IUserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    @Query("SELECT u FROM User u JOIN FETCH u.userFullData WHERE u.userFullData.email = :email")
+    Optional<User> findByUserFullData_Email(String email);
+    Optional<User> findByUsernameOrUserFullData_Email(String username, String email);
+
+    @Query("SELECT u FROM User u JOIN FETCH u.userFullData WHERE u.username = :username")
+    Optional<User> findByUsernameWithFullData(@Param("username") String username);
 }
