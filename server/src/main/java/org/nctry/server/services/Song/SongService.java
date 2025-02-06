ced09b0dbab63c844505b79f6606069492494549
amoroccoire <amoroccoire@unsa.edu.pe>
2025-02-06 16:44:16 -0500

    agregando builders quitando mappers

--- server/src/main/java/org/nctry/server/services/Song/SongService.java
+++ server/src/main/java/org/nctry/server/services/Song/SongService.java
@@ -1,11 +1,45 @@
 package org.nctry.server.services.Song;
 
+import org.nctry.server.Utilities.Pages.response.GeneralResponse;
+import org.nctry.server.song.dto.response.DtoSong;
+import org.springframework.cache.annotation.Cacheable;
+
 public interface SongService {
-    void /*SongDTO*/ saveSong(/*SongDTO songDTO*/);
-    void deleteSong();
+    //@Cacheable("all-songs")
+    GeneralResponse getAllActiveSongs(
+            Integer pageNumber,
+            Integer pageSize,
+            String sortBy,
+            String sortDir);
+
+    //@Cacheable("all-songs-byName")
+    GeneralResponse getAllActiveSongsByName(
+            String name,
+            Integer pageNumber,
+            Integer pageSize,
+            String sortBy,
+            String sortDir);
+
+    //@Cacheable("all-songs-byArtist")
+    GeneralResponse getAllActiveSongsByArtist(
+            String artistName,
+            Integer pageNumber,
+            Integer pageSize,
+            String sortBy,
+            String sortDir);
+
+
+    /*DtoSong getSongById(Long id);
+
+    DtoSong saveSong(DtoSong dtoNewSong);
+
+    void deleteSongByNameAndArtist(DtoSong dtoSong);
+
     void deleteSongById(Long id);
-    void getSongById(Long id);
-    void /*List<SongDTO>*/ getAllSong();
-    void /*List<SongDTO>*/ getAllSongByName(String name);
-    void likeSong(Long userId, Long songId);
+
+    void wipeSongById(Long id);
+
+    void wipeSongByNameAndArtist(DtoSong dtoSong);
+
+    void likeSong(Long userId, Long songId);*/
 }
